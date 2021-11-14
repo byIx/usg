b81063e43ccf315c547b42fdfcfbba67c62901dc
byIx <40878600+byIx@users.noreply.github.com>
2021-11-15 02:56:47 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.1.0", "longversion":11000, "released":1636916400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2491-k4-info-to-postcontainer\/" }
+{ "version":"1.1.0", "longversion":11000, "released":1636916400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2599-wk-user-steam-group\/" }
